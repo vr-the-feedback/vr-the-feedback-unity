e0846ffa993bdfc3a4e5b143fb6cea2ccb550819
Greg <gdziemidowicz@gmail.com>
2016-12-27 09:30:36 +1100

    first commit - we are the feedback

---
+++ Assets/VRTheFeedback/Scripts/FeedbackDemoUIController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FeedbackDemoUIController : MonoBehaviour {
+
+    public Text instruction;
+    public VRTheFeedbackManager vrTheFeedbackManager;
+    public Button recordFeedback;
+    public Button uploadFeedback;
+
+    public void Start()
+    {
+        recordFeedback.enabled = true;
+        uploadFeedback.enabled = false;
+    }
+
+    public void OnClickRecordFeedback()
+    {
+        Debug.Log("OnClickRecordFeedback");
+        recordFeedback.enabled = false;
+        uploadFeedback.enabled = true;
+        instruction.text = "Please talk - record test feedback.";
+        vrTheFeedbackManager.RecordFeedback();
+    }
+
+    public void OnClickUploadFeedback()
+    {
+        Debug.Log("OnClickUploadFeedback");
+        recordFeedback.enabled = true;
+        uploadFeedback.enabled = false;
+    }
+
+}
---
+++ Assets/VRTheFeedbackManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRTheFeedbackManager : MonoBehaviour {
+
+    private AudioSource myAudio;
+
+	// Use this for initialization
+	void Start () {
+        myAudio = GetComponent<AudioSource>();
+
+    }
+	
+    public void RecordFeedback()
+    {
+        myAudio.clip = Microphone.Start(null, false, 10, 44100);
+    }
+
+}
