9ce8d48f042108175de08a975eb7c674988f21b2
Greg <gdziemidowicz@gmail.com>
2016-12-27 20:06:57 +1100

    dealing with windows

--- Assets/VRTheFeedback/Scripts/VRTheFeedbackManager.cs
+++ Assets/VRTheFeedback/Scripts/VRTheFeedbackManager.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using System.IO;
 using LitJson;
 using System.Net;
-using System.Threading; 
-
+using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+using System;
 
 public class PresignedResponseJSON
 {
@@ -24,7 +26,33 @@ public class VRTheFeedbackManager : MonoBehaviour {
 	private float[] justFeedbackSamples;
 	public AudioClip justFeedback;
 
-	void Start () {
+    public bool MyRemoteCertificateValidationCallback(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+    {
+        bool isOk = true;
+        // If there are errors in the certificate chain, look at each error to determine the cause.
+        if (sslPolicyErrors != SslPolicyErrors.None)
+        {
+            for (int i = 0; i < chain.ChainStatus.Length; i++)
+            {
+                if (chain.ChainStatus[i].Status != X509ChainStatusFlags.RevocationStatusUnknown)
+                {
+                    chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+                    chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                    chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+                    chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+                    bool chainIsValid = chain.Build((X509Certificate2)certificate);
+                    if (!chainIsValid)
+                    {
+                        isOk = false;
+                    }
+                }
+            }
+        }
+        return isOk;
+    }
+
+    void Start () {
+        ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
         myAudio = GetComponent<AudioSource>();
 		saveWav = new SavWav ();
     }
@@ -36,9 +64,22 @@ public class VRTheFeedbackManager : MonoBehaviour {
 
     public void SaveFeedback()
     {
-		Microphone.End (null);
-		filePath = Path.Combine (Application.persistentDataPath, "test.mp3");
-		justFeedback = saveWav.TrimSilence (myAudio.clip, 0.01f);
+        int lastTime = Microphone.GetPosition(null);
+        if (lastTime == 0) return;
+        Microphone.End (null);
+        float[] samples = new float[myAudio.clip.samples];
+        myAudio.clip.GetData(samples, 0);
+        float[] ClipSamples = new float[lastTime];
+        Array.Copy(samples, ClipSamples, ClipSamples.Length - 1);
+        AudioClip newClip = AudioClip.Create("playRecordClip", ClipSamples.Length, 1, 44100, false, false);
+        newClip.SetData(ClipSamples, 0);
+
+        AudioClip.Destroy(myAudio.clip);
+        myAudio.clip = newClip;
+
+        filePath = Path.Combine (Application.persistentDataPath, "test.mp3");
+		justFeedback = saveWav.TrimSilence (myAudio.clip, 0.001f);
+        //saveWav.Save(filePath + ".wav", justFeedback);
 
 		if (filePath != null) {
 			StartCoroutine(UploadToServer ());
@@ -69,7 +110,8 @@ public class VRTheFeedbackManager : MonoBehaviour {
 		_threadRunning = true;
 		Debug.Log("Starting upload on separate thread.");
 		EncodeMP3.convert(justFeedbackSamples, filePath, 128);
-		UploadObject (json.url, filePath);
+        Debug.Log("Find mp3 file under: " + filePath);
+        UploadObject (json.url, filePath);
 		justFeedbackSamples = null;
 		justFeedback = null;
 		Debug.Log("Upload done..");
@@ -87,22 +129,29 @@ public class VRTheFeedbackManager : MonoBehaviour {
 
 	static void UploadObject(string url, string filePath)
 	{
-		HttpWebRequest httpRequest = WebRequest.Create(url) as HttpWebRequest;
+        try {
+        HttpWebRequest httpRequest = WebRequest.Create(url) as HttpWebRequest;
 		httpRequest.Method = "PUT";
 		using (Stream dataStream = httpRequest.GetRequestStream())
 		{
 			byte[] buffer = new byte[8000];
 			using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
 			{
-				int bytesRead = 0;
+                int bytesRead = 0;
 				while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
 				{
-					dataStream.Write(buffer, 0, bytesRead);
+                    dataStream.Write(buffer, 0, bytesRead);
 				}
-			}
+            }
 		}
 		HttpWebResponse response = httpRequest.GetResponse() as HttpWebResponse;
-	}
+        response.Close();
+        Debug.Log("Response from server: " + response.StatusCode);
+        } catch (System.Exception ex)
+        {
+            Debug.LogError("something went wrong.. " + ex);
+        }
+    }
 
 
 	void OnDisable()
