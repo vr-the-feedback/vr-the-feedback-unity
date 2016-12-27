9e1b2401da89ba5a635d468d89428cff1f4280c0
Greg Dziemidowicz <greg@cogent.co>
2016-12-27 18:17:30 +1100

    moved things around

--- Assets/VRTheFeedback/Scripts/SavWav.cs
+++ Assets/VRTheFeedback/Scripts/SavWav.cs
@@ -154,7 +154,6 @@ public class SavWav
         Byte[] subChunk1 = BitConverter.GetBytes(16);
         fileStream.Write(subChunk1, 0, 4);
 
-        UInt16 two = 2;
         UInt16 one = 1;
 
         Byte[] audioFormat = BitConverter.GetBytes(one);
